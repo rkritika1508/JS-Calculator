76b63732fc55f938f174ef9487f68cd203078bf3
Kritika <rkritika1508@gmail.com>
2018-02-25 23:30:25 +0530

    Still trying

--- calc.js
+++ calc.js
@@ -2,14 +2,25 @@
 var number = document.querySelectorAll(".num");
 var operator = document.querySelectorAll(".operator");
 var content = document.querySelector("textarea");
+var num1;
 // for (var i = 0; i < number.length; i++) {
 //  console.log(number[i].value);
 // }
 
 for(var i=0;i<number.length;i++){
   	number[i].addEventListener("click", function(){
-  		//console.log(this.value);
   		var numr = this.value;
   		content.textContent = content.textContent + numr;
+  		num1 = Number(content.textContent);
 	})
 }
+content.textContent = "";
+console.log(num1);
+
+for(var j=0;j<operator.length;j++){
+	operator[j].addEventListener("click", function(){
+		content.textContent = this.value;
+	})
+}
+content.textContent = "";
+
