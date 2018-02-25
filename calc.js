c76d364969fea86ad90d0318886ecc987059ac5c
Kritika <rkritika1508@gmail.com>
2018-02-26 02:32:14 +0530

    Trying to resolve the errors

--- calc.js
+++ calc.js
@@ -4,16 +4,19 @@ var operator = document.querySelectorAll(".operator");
 var content = document.querySelector("textarea");
 var ac = document.querySelector("#allclear");
 var clear = document.querySelector("#clear");
-var num1, ops;
-// for (var i = 0; i < number.length; i++) {
-//  console.log(number[i].value);
-// }
+var oppsign = document.querySelector("#oppsign");
+var num1, ops, opsIndex,result;
+var plus = document.getElementById("+");
+var minus = document.getElementById("-");
+var multiply = document.getElementById("*");
+var divide = document.getElementById("/");
 
 for(var i=0;i<number.length;i++){
   	number[i].addEventListener("click", function(){
   		var numr = this.value;
   		content.textContent = content.textContent + numr;
-  		num1 = Number(content.textContent);
+  		num1 = Number(content.textContent.substr(0,opsIndex));
+  		num2 = Number(content.textContent.substr(opsIndex+1));
 	})
 }
 
@@ -21,12 +24,37 @@ for(var j=0;j<operator.length;j++){
 	operator[j].addEventListener("click", function(){
 		ops = this.value;
 		content.textContent+=ops;
+		opsIndex = content.textContent.indexOf(ops);
 	})
 }
+
 ac.addEventListener("click", function(){
 	content.textContent = "";
+	num1 = 0;
+	num2 = 0;
+	result = 0;
 })
 
 clear.addEventListener("click", function(){
 	content.textContent = content.textContent.slice(0,-1);
 })
+
+function add(){
+	plus.addEventListener("click", function(){
+	result = num1+num2;
+})
+}
+
+
+
+// minus.addEventListener("click", function(){
+// 	result = num1-num2;
+// })
+
+// multiply.addEventListener("click", function(){
+// 	result = num1*num2;
+// })
+
+// divide.addEventListener("click", function(){
+// 	result = num1/num2 ;
+// })
